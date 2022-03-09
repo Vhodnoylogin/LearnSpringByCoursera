314e3e5a6469f31d0680a75be6990c9fe6e4f3c2
Radimir Karmatskiy <rkarmatskiy@phoenixit.ru>
2022-03-09 13:35:14 +0300

    initial commit

---
+++ src/main/java/com/exapmle/simplewebapplication/SimpleController.java
@@ -0,0 +1,13 @@
+package com.exapmle.simplewebapplication;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "HELLP";
+    }
+}
