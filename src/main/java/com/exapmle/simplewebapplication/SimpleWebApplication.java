8d7e21e801f4bbef8837e7ec997aca4638d14772
Radimir Karmatskiy <rkarmatskiy@phoenixit.ru>
2022-03-09 13:44:58 +0300

    go to example

--- src/main/java/com/exapmle/simplewebapplication/SimpleWebApplication.java
+++ src/main/java/com/exapmle/simplewebapplication/SimpleWebApplication.java
@@ -1,7 +1,13 @@
 package com.exapmle.simplewebapplication;
 
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class SimpleWebApplication {
@@ -10,4 +16,15 @@ public class SimpleWebApplication {
 		SpringApplication.run(SimpleWebApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext context){
+		return args -> {
+			System.out.println("Let's inspect the beans provide by Spting Boot:");
+			String[] beans = context.getBeanDefinitionNames();
+			Arrays.sort(beans);
+			for (String bean : beans) {
+				System.out.println(bean);
+			}
+		};
+	}
 }
